8c470e5827b86b098e18ef2f0967a1134f0760d7
mozzy69 <mozzy69@yahoo.com>
2021-01-18 15:20:34 +0200

    general footer styling

--- scripts/kanba.js
+++ scripts/kanba.js
@@ -7,11 +7,11 @@ var flkty = new Flickity( '.main-gallery', {
 
 
 window.onscroll = function(){
-    stickyNav()
+    stickyNav();
 };
 
 // Get the navbar
-let navbar = document.getElementById("mainNav");
+let navbar = document.getElementById("navbar");
 
 // Get the offset position of the navbar
 let sticky = navbar.offsetTop;
@@ -19,7 +19,7 @@ let sticky = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyNav() {
   if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+    navbar.classList.add("sticky"); 
   } else {
     navbar.classList.remove("sticky");
   }
